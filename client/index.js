168bac39bf11e09bc9b2bbfe519fab3b8702c7ae
0xJayPi <jpcampaya@gmail.com>
2022-12-19 15:12:19 +0100

    Excersice completed

--- client/index.js
+++ client/index.js
@@ -1,17 +1,32 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
+// const { useState } = require("react");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+const merkleTree = new MerkleTree(niceList);
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  const root = merkleTree.getRoot();
+  const name = niceList[10];
+  const index = niceList.findIndex((n) => n === name);
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+  const proof = merkleTree.getProof(index);
 
-  console.log({ gift });
+  // const [gift, setGift] = useState(0);
+  const body = {
+    root: root,
+    name: name,
+    proof: proof,
+  };
+
+  try {
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, body);
+    console.log({ gift });
+  } catch (e) {
+    console.log(`Error: ${e}`);
+  }
 }
 
 main();
